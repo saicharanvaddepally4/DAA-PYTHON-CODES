31a6b3d2b38e2f897ee88c20f41da4caea64d629
saicharanvaddepally4 <saicharanvaddepally4@gmail.com>
2026-07-30 22:12:18 +0530

    Update linearsearch.py

--- linearsearch.py
+++ linearsearch.py
@@ -1,1 +1,9 @@
+a = [10, 20, 30, 40, 50]
+x = int(input("Enter number: "))
 
+for i in range(len(a)):
+    if a[i] == x:
+        print("Found at index", i)
+        break
+else:
+    print("Not Found")
