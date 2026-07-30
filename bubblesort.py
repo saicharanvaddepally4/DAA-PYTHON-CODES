18adf2deb385c02282f643aed59b1d46f565325a
saicharanvaddepally4 <saicharanvaddepally4@gmail.com>
2026-07-30 22:23:27 +0530

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,1 +1,8 @@
+arr = [5, 3, 8, 4, 2]
 
+for i in range(len(arr)):
+    for j in range(len(arr) - 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+print("Sorted Array:", arr)
