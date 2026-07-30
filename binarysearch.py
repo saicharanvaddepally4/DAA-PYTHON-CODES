d3d30c3bcdedabcc9eabaf19a66eb42463065b3e
saicharanvaddepally4 <saicharanvaddepally4@gmail.com>
2026-07-30 22:20:37 +0530

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -1,1 +1,18 @@
+arr = [10, 20, 30, 40, 50]
+x = 30
 
+low = 0
+high = len(arr) - 1
+
+while low <= high:
+    mid = (low + high) // 2
+
+    if arr[mid] == x:
+        print("Element found at index", mid)
+        break
+    elif arr[mid] < x:
+        low = mid + 1
+    else:
+        high = mid - 1
+else:
+    print("Element not found")
