fed50756f8243c1d991a55eab5ffa425e2cea7a8
saicharanvaddepally4 <saicharanvaddepally4@gmail.com>
2026-07-30 22:26:02 +0530

    Update selectionsort.py

--- selectionsort.py
+++ selectionsort.py
@@ -1,1 +1,10 @@
+arr = [64, 25, 12, 22, 11]
 
+for i in range(len(arr)):
+    min = i
+    for j in range(i + 1, len(arr)):
+        if arr[j] < arr[min]:
+            min = j
+    arr[i], arr[min] = arr[min], arr[i]
+
+print(arr)
