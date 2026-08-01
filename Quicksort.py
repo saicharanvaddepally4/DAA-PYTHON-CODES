70c42761838dfc41a3aef3bb2b1775df93351298
saicharanvaddepally4 <saicharanvaddepally4@gmail.com>
2026-08-01 17:11:35 +0530

    Create Quicksort.py

---
+++ Quicksort.py
@@ -0,0 +1,21 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[0]
+    left = []
+    right = []
+
+    for i in arr[1:]:
+        if i <= pivot:
+            left.append(i)
+        else:
+            right.append(i)
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+# Main Program
+arr = list(map(int, input("Enter numbers: ").split()))
+
+print("Before Sorting:", arr)
+print("After Sorting:", quick_sort(arr))
